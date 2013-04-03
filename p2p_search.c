169823ba15a92a66a6c4b9ff0d47d2078be33ac5
alexisd61@gmail.com <alexisd61@gmail.com>
2013-04-03 15:10:18 +0200

    add search functions file

---
+++ p2p_search.c
@@ -0,0 +1,29 @@
+/* 
+ * File:   p2p_search.h
+ * Author: alexis
+ *
+ * Created on 3 avril 2013, 14:45
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h> 
+#include <sys/types.h> 
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <netdb.h> 
+#include <assert.h>
+#include <arpa/inet.h>
+#include <inttypes.h>
+
+
+#include "p2p_common.h"
+#include "p2p_addr.h"
+#include "p2p_msg.h"
+#include "p2p_options.h"
+#include "p2p_do_msg.h"
+#include "p2p_file.h"
+
+
+// Put usefull SEARCH function here
---
+++ p2p_search.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   p2p_search.h
+ * Author: alexis
+ *
+ * Created on 3 avril 2013, 14:45
+ */
+
+
+#ifndef P2P_SEARCH_H
+#define	P2P_SEARCH_H
+
+
+#endif	/* P2P_SEARCH_H */
+
