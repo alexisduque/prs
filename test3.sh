00e1bd09eb535bbb80098f716c06a5d6e6e5c334
Alexis <alexisd61@gmail.com>
2013-05-23 08:09:02 +0200

    adding test files

---
+++ test3.sh
@@ -0,0 +1,9 @@
+sudo ./start_node_ref.sh x 3
+sudo ./start_node_ref.sh c 0
+sudo ./start_node_ref.sh c 1
+sudo ./start_node_ref.sh c 2
+sudo ./start_node.sh x 2
+sudo ./start_node.sh c 0
+sudo ./start_node.sh c 1
+
+#sudo  ./src-dist/p2p_node --verbose=4
