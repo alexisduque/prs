50292cdc80ef8ac2c4cabb5768fc924c39b7885c
Alexis <alexisd61@gmail.com>
2013-05-29 10:47:13 +0200

    Final Versio n v1.3 - Working !

--- test1.sh
+++ test1.sh
@@ -1,8 +1,7 @@
-sudo ./start_node.sh x 2
-sudo ./start_node.sh c 0
-sudo ./start_node.sh c 1
-
-sudo ./start_node_ref.sh x 1
-sudo ./start_node_ref.sh c 0
+./start_node.sh x 2
+./start_node.sh c 0
+./start_node.sh c 1
+./start_node_ref.sh x 1
+./start_node_ref.sh c 0
 
 #sudo  ./src-dist/p2p_node --verbose=4
